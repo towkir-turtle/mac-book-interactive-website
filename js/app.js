db8a34208b895668c84b3de083780a3dd5928e79
Towkir Ahmed <towkir_ahmed@yahoo.com>
2021-08-21 03:10:07 +0600

    second commit done

--- js/app.js
+++ js/app.js
@@ -1,27 +1,76 @@
 //Add memory button event handler:
-function getMemoryInput(memoryPrice){
-    const memoryCost = document.getElementById('memory-cost');
+function getItemInput(item, memoryPrice){
+    const memoryCost = document.getElementById(item + '-cost');
     memoryCost.innerText = memoryPrice;
+
+    getCost();
+
+    getTotal();
 }
 document.getElementById('default-memory-input').addEventListener('click', function(){    
-    getMemoryInput(0);
+    getItemInput('memory', 0);
 });
 document.getElementById('extra-memory-input').addEventListener('click', function(){    
-    getMemoryInput(180);
+    getItemInput('memory', 180);
 });
 
 //Add storage button event handler:
-function getStorageInput(storagePrice){
-    const storageCost = document.getElementById('storage-cost');
-    storageCost.innerText = storagePrice;
-}
 document.getElementById('default-storage-input').addEventListener('click', function(){
-    getStorageInput(0);
+    getItemInput('storage', 0);
 });
 document.getElementById('extra-storage-input').addEventListener('click', function(){
-    getStorageInput(100);
+    getItemInput('storage', 100);
 });
 document.getElementById('ultra-storage-input').addEventListener('click', function(){
-    getStorageInput(180);
+    getItemInput('storage', 180);
 });
+
+//Add delivery button event handler:
+document.getElementById('normal-delivery-charge').addEventListener('click', function(){
+    getItemInput('delivery', 0);
+});
+document.getElementById('fast-delivery-charge').addEventListener('click', function(){
+    getItemInput('delivery', 20);
+});
+
+//get cost:
+function getCost(){
+    const bestCostInput = document.getElementById('best-cost');
+    const bestInput = parseInt(bestCostInput.innerText);
+    
+    const memoryCostInput = document.getElementById('memory-cost');
+    const memoryInput = parseInt(memoryCostInput.innerText);
+
+    const storageCostInput = document.getElementById('storage-cost');
+    const storageInput = parseInt(storageCostInput.innerText);
+
+    const deliveryCostInput = document.getElementById('delivery-cost');
+    const deliveryInput = parseInt(deliveryCostInput.innerText);
+
+    const totalPriceInput = document.getElementById('total-price');
+    totalPriceInput.innerText = bestInput + memoryInput + storageInput + deliveryInput;
+
+    return totalPriceInput;
+}
+
+function getTotal(){
+    const bestCostInput = document.getElementById('best-cost');
+    const bestInput = parseInt(bestCostInput.innerText);
+    
+    const memoryCostInput = document.getElementById('memory-cost');
+    const memoryInput = parseInt(memoryCostInput.innerText);
+
+    const storageCostInput = document.getElementById('storage-cost');
+    const storageInput = parseInt(storageCostInput.innerText);
+
+    const deliveryCostInput = document.getElementById('delivery-cost');
+    const deliveryInput = parseInt(deliveryCostInput.innerText);
+
+    const totalPriceInput = document.getElementById('total');
+    totalPriceInput.innerText = bestInput + memoryInput + storageInput + deliveryInput;
+
+    return totalPriceInput;
+};
+
+
 
